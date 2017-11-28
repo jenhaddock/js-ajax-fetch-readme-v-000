3f2029a902bde3b906e8dafc63371088a4c2c3d8
Jennifer Haddock <jentx123@hotmail.com>
2017-11-28 03:53:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'e1c3668f22a2804516855f7b33bc0920b1a28e48'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
